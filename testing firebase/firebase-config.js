ab9550a0607230d06ef7fa07531ca1cbd055028d
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-05-06 18:41:21 +0300

    trying out firebase

---
+++ testing firebase/firebase-config.js
@@ -0,0 +1,12 @@
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: "AIzaSyD88DvO9KuE6NKj_-RDYSZ0vDTYikPgy4U",
+  authDomain: "graphql-c3e87.firebaseapp.com",
+  projectId: "graphql-c3e87",
+  storageBucket: "graphql-c3e87.appspot.com",
+  messagingSenderId: "162702018611",
+  appId: "1:162702018611:web:8cf735e1b78230a18ebb46",
+};
+
+firebase.initializeApp(firebaseConfig);
