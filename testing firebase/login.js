28c057d0b0d9d2f89fb35bc9e13f50a5bd2e3f0c
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-05-06 18:45:36 +0300

    firebase testing

---
+++ testing firebase/login.js
@@ -0,0 +1,37 @@
+// Get a reference to the Firebase Authentication service
+
+
+
+const auth = firebase.auth();
+
+function login() {
+  const email = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      alert("Login Success");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+
+  // return false;
+  // alert("helo");
+}
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in
+    const uid = user.uid;
+    console.log(uid);
+  } else {
+    // User is signed out
+  }
+});
