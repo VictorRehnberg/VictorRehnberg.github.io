f7454bf066daf9a5754242f317c00636740b385d
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-05-07 18:17:32 +0300

    more advanced query with xp, audit ratio

--- js/profile.js
+++ js/profile.js
@@ -11,15 +11,47 @@ async function fetchData() {
   }
 
   const query = `
-  query {
-    user {
-      id
-      login
-      attrs
-      campus
+    query {
+      user {
+        login
+        attrs
+        campus
+        upAmount: transactions_aggregate(where: {type: {_eq: "up"}}) {
+          aggregate {
+            sum {
+              amount
+            }
+          }
+        }
+        downAmount: transactions_aggregate(where: {type: {_eq: "down"}}) {
+          aggregate {
+            sum {
+              amount
+            }
+          }
+        }
+        xpAmount: transactions_aggregate(
+          where: {
+            type: { _eq: "xp" }
+            _or: [
+              { attrs: { _eq: {} } }
+              { attrs: { _has_key: "group" } }
+            ]
+            _and: [
+              { path: { _nlike: "%/piscine-js/%" } }
+              { path: { _nlike: "%/piscine-go/%" } }
+            ]
+          }
+        ) {
+          aggregate {
+            sum {
+              amount
+            }
+          }
+        }
+      }
     }
-  }
-`;
+  `;
 
   try {
     const response = await fetch(
@@ -37,33 +69,52 @@ async function fetchData() {
     const data = await response.json();
     const user = data.data.user[0];
     displayUserInfo(user);
-    // generateGraphs(user);
+    displayUserXp(
+      user.xpAmount.aggregate.sum.amount,
+      user.upAmount.aggregate.sum.amount,
+      user.downAmount.aggregate.sum.amount
+    );
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 }
 
+function displayUserXp(xpAmount, upAmount, downAmount) {
+  document.querySelector(".xp-ratio .xp-value").textContent =
+    // remove decimals and round to nearest integer
+    Math.round((upAmount / downAmount) * 10) / 10;
+
+  document.getElementById(
+    "total-xp"
+  ).textContent = `Total XP: ${convertToByteUnits(xpAmount)}`;
+
+  document.querySelector(".xp-up .xp-value").textContent =
+    convertToByteUnits(upAmount);
+
+  document.querySelector(".xp-down .xp-value").textContent =
+    convertToByteUnits(downAmount);
+}
+
 function displayUserInfo(user) {
+  // Set the title of the page to the username of the user
+  document.title = `${user.login}s Profile`;
   // Set the user image
-
-  // if (user.attrs.image) {
   document.querySelector(".user-image").src = user.attrs.image;
-  // }
-
+  // set the user name
   document.getElementById(
     "name-profile"
   ).textContent = ` ${user.login}s Profile`;
-
+  // Set the user phone number
   document.getElementById("phone").textContent = ` ${user.attrs.phonenumber}`;
-
+  //  set the user email
   document.getElementById("email").textContent = ` ${user.attrs.email}`;
-
+  // set the user first name and last name
   document.getElementById(
     "first-name-last-name"
   ).textContent = ` ${user.attrs.firstName} ${user.attrs.lastName}`;
-
+  // set the user campus
   document.getElementById("campus").textContent = ` Student at ${user.campus}`;
-
+  // set the user age and country
   document.getElementById("from").textContent = `${calculateAge(
     user.attrs.dateOfBirth
   )} Years old from ${user.attrs.country}`;
@@ -79,20 +130,17 @@ function calculateAge(dateOfBirthStr) {
   return Math.abs(ageDate.getUTCFullYear() - 1970); // subtract 1970 to get the age in years
 }
 
-function logout() {
-  localStorage.removeItem("jwt");
-  window.location.href = "/login.html";
-}
-
 // Call fetchData on page load
 fetchData();
 
 function convertToByteUnits(num) {
   const units = ["bytes", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
   let i = 0;
-  while (num >= 1024 && i < units.length - 1) {
-    num /= 1024;
+  while (num >= 1000 && i < units.length - 1) {
+    num /= 1000;
     i++;
   }
-  return num.toFixed(2) + " " + units[i];
+  // remove decimals and round up to nearest integer
+  num = Math.round(num);
+  return `${num} ${units[i]}`;
 }
